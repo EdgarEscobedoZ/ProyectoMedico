d5daaa18dc72c3038cef21a3ad92ef41a9656b06
PJazminCastro <125835937+PJazminCastro@users.noreply.github.com>
2023-06-24 08:19:57 -0600

    Se agregaron los nombres de la BD, y la ruta de registro de pacientes

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MySQL_HOST']='localhost'
 app.config['MySQL_USER']='root'
-app.config['MySQL_PASSWORD']='root'
-app.config['MySQL_DB']='bdflask'
+app.config['MySQL_PASSWORD']=''
+app.config['MySQL_DB']='bdcentromedico'
 app.secret_key='mysecretkey'
 mysql=MySQL(app)
 
@@ -13,22 +13,26 @@ mysql=MySQL(app)
 def index():
     return render_template('login.html')
 
-
-
-@app.route('/registro', methods=('POST'))
+@app.route('/registroMedico', methods=('POST'))
 def registrarMedico():
     if request.method == 'POST':
         VRFC = request.form['RFC']
-        VNOMBRE = request.form['Nombre']
+        VNOMBRE = request.form['nombre_completo']
         VCEDULA = request.form['cedula']
         VCORREO = request.form['correo']
         VCONTRASEÑA = request.form['contraseña']
-        print(VRFC, VNOMBRE, VCEDULA, VCORREO, VCONTRASEÑA)
-    return 'Los datos llegaron'
+        VROL = request.form['rol']
+        print(VRFC, VNOMBRE, VCEDULA, VCORREO, VCONTRASEÑA, VROL)
+    return 'Los datos llegaron correctamente'
 
-
-            
-
-
-
-
+@app.route('/registroPaciente', methods=('POST'))
+def registrarMedico():
+    if request.method == 'POST':
+        VMEDICO = request.form['medico']
+        VNOMBRE = request.form['nombre_completo']
+        VFECHANAC = request.form['fecha_nacimiento']
+        VENFERMEDAD = request.form['enfermedades_cronicas']
+        VALERGIAS = request.form['alergias']
+        VANTECEDENTES = request.form['antecedentes_familiares']
+        print(VMEDICO, VNOMBRE, VFECHANAC, VENFERMEDAD, VALERGIAS, VANTECEDENTES)
+    return 'Los datos llegaron correctamente'
